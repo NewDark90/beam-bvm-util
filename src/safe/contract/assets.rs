c669b337d5da9d020ea3079fa81a34f6983515e4
Zachary Dow <mr.zachary.dow@gmail.com>
2022-09-18 22:51:17 -0700

    asset functions

--- src/safe/contract/assets.rs
+++ src/safe/contract/assets.rs
@@ -1,6 +1,5 @@
 use beam_bvm_interface::root::*;
 
-
 pub fn funds_lock(aid: AssetID, amount: Amount) {
     unsafe { Env::FundsLock(aid, amount) }
 }
@@ -11,4 +10,16 @@ pub fn funds_unlock(aid: AssetID, amount: Amount) {
 
 pub fn add_sig(pubkey: &PubKey) {
     unsafe { Env::AddSig(pubkey) }
+}
+
+pub fn asset_create(meta_ptr: *const c_void, meta_size: u32) -> AssetID {
+    unsafe { Env::AssetCreate(meta_ptr, meta_size) }
+}
+
+pub fn asset_emit(id: AssetID, amount: Amount, emit: u8) -> u8 {
+    unsafe { Env::AssetEmit(id, amount, emit) }
+}
+
+pub fn asset_destroy(id: AssetID) -> u8 {
+    unsafe { Env::AssetDestroy(id) }
 }
