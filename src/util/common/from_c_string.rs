1cd11f7e81b90561199486b4e2f7d01c2fc933a7
Zachary Dow <mr.zachary.dow@gmail.com>
2022-10-06 18:23:59 -0700

    using the cstr c_char and using either impl for i8/u8

--- src/util/common/from_c_string.rs
+++ src/util/common/from_c_string.rs
@@ -1,29 +1,41 @@
 use core::str::Utf8Error;
 
-use beam_bvm_interface::root::{c_char};
-use cstr_core::{ CStr};
+use cstr_core::{ CStr, c_char};
 
 pub trait FromCString {
     fn from_c_string(&self) -> Result<&'static str, Utf8Error>;
 }
 
-
-impl FromCString for *const c_char {
-    fn from_c_string(self: &*const c_char) -> Result<&'static str, Utf8Error> {
+impl FromCString for *const i8 {
+    fn from_c_string(self: &*const i8) -> Result<&'static str, Utf8Error> {
         unsafe {
-            CStr::from_ptr((*self) as *const i8).to_str()
+            CStr::from_ptr((*self) as *const c_char).to_str()
         }
     }
 }
 
-impl FromCString for *mut c_char {
-    fn from_c_string(self: &*mut c_char) -> Result<&'static str, Utf8Error> {
+impl FromCString for *mut i8 {
+    fn from_c_string(self: &*mut i8) -> Result<&'static str, Utf8Error> {
         unsafe {
-            CStr::from_ptr((*self) as *mut i8).to_str()
+            CStr::from_ptr((*self) as *mut c_char).to_str()
         }
     }
 }
 
+impl FromCString for *const u8 {
+    fn from_c_string(self: &*const u8) -> Result<&'static str, Utf8Error> {
+        unsafe {
+            CStr::from_ptr((*self) as *const c_char).to_str()
+        }
+    }
+}
 
+impl FromCString for *mut u8 {
+    fn from_c_string(self: &*mut u8) -> Result<&'static str, Utf8Error> {
+        unsafe {
+            CStr::from_ptr((*self) as *mut c_char).to_str()
+        }
+    }
+}
 
 
